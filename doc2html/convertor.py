c45aa15dca4531bbd8b946254968887790223896
uxid <xudi>
2020-01-31 19:17:07 +0800

    init

---
+++ doc2html/convertor.py
@@ -0,0 +1,56 @@
+#coding:utf-8
+
+import ConfigParser as cp
+import misc
+import os
+from bs4 import BeautifulSoup
+
+def process_title(html_path):
+    fname = '.'.join(html_path.split('\\')[-1].split('.')[:-1]).decode('cp936')
+    print html_path.decode('cp936')
+    with open(html_path,'r') as fin:
+        buffer = ''.join(fin.readlines())
+        soup = BeautifulSoup(buffer,'lxml')
+        head_tag = soup.find('head')
+        title_tags = head_tag.find_all('title')
+        for t in title_tags:
+            t.extract()
+        misc.insert_tag(head_tag,'title', fname)
+    with open(html_path,'w') as fout:
+        fout.write(soup.encode('cp936'))    
+
+def doc2html(src_path,des_path):
+    src_path_length = len(src_path.split(os.path.sep))
+    scnt,ecnt = 0,0
+    for root_dir,sub_dir,files in os.walk(src_path):
+        infiles = [ifile for ifile in files if ( not ifile.startswith('~') and (ifile.endswith('docx') or ifile.endswith('doc')))]
+        cur_dir = os.path.sep.join((root_dir.split(os.sep)[src_path_length:]))
+        des_dir = os.path.join(des_path,cur_dir)
+        if not os.path.exists(des_dir):
+            os.makedirs(des_dir)
+        print cur_dir.decode('cp936')
+        for ifile in infiles:
+            doc_path = os.path.join(root_dir,ifile)
+            html_path = os.path.join(des_dir,'.'.join(ifile.split('.')[:-1]) + '.html')
+            if os.path.exists(html_path):
+                continue
+            else:
+                try:
+                    misc.doc2html(doc_path, html_path)
+                    scnt += 1
+                    print scnt,'\t',os.path.join(cur_dir,ifile).decode('cp936')
+                    process_title(html_path)
+                except:
+                    ecnt += 1
+                    print 'error! infile = ' , os.path.join(cur_dir,ifile).decode('cp936')
+    print 'successed conveted ' + str(scnt) + ' documents!' 
+    print 'failed conveted ' + str(ecnt) + ' documents!' 
+    misc.word.Quit()
+        
+if __name__ == '__main__':
+    config = cp.ConfigParser()
+    config.read('config.txt')
+    src_path = config.get('config','src_path')
+    des_path = config.get('config','des_path')
+    doc2html(src_path,des_path)
+    
---
+++ doc2html/misc.py
@@ -0,0 +1,28 @@
+#coding:utf-8
+
+from win32com.client import Dispatch as handler
+from bs4 import BeautifulSoup
+
+word = handler(('Word.Application'))
+word.Visible = False
+word.DisplayAlerts = False
+
+def doc2html(doc_path,html_path):
+    doc = word.Documents.Open(doc_path)
+    doc.SaveAs(html_path,10)
+    doc.Close()
+    
+def html2doc(html_path,doc_path):
+    doc = word.Documents.Open(html_path)
+    if doc_path.endswith('docx'):
+        doc.SaveAs(doc_path,12)
+    else:
+        doc.SaveAs(doc_path,0)
+        
+def str2tag(s):
+    return BeautifulSoup(s,'lxml')
+
+def insert_tag(parent,tag_name,content):
+    new_tag = str2tag('<' + tag_name + '>' + '</' + tag_name + '>').find(tag_name)
+    new_tag.string = content.decode('cp936') if not isinstance(content, unicode) else content 
+    parent.insert(0,new_tag)
